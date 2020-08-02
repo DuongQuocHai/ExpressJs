624301a293b10f969a6b833fe595e4234e78d99e
= <=>
2020-08-02 17:17:26 +0700

    express lesson 3 - Query parameters

--- WebServerNodeJS&&ExpressJS/index.js
+++ WebServerNodeJS&&ExpressJS/index.js
@@ -11,25 +11,39 @@ const port = 3000
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+const todos = [
+    { id: 1, todo: 'Đi chợ' },
+    { id: 2, todo: 'Nấu cơm' },
+    { id: 3, todo: 'Rửa bát' },
+    { id: 4, todo: 'Học tại CodersX' }
+]
 
-app.get('/', function(req, res){
+
+app.get('/', function (req, res) {
     res.render("index", {
         name: 'Hai'
     });
 })
 
-app.get('/todos', function(req, res){
-    res.render('./todos/index',{
-        todos:[
-            {id:1, todo:'Đi chợ'},
-            {id:2, todo:'Nấu cơm'},
-            {id:3, todo:'Rửa bát'},
-            {id:4, todo:'Học tại CodersX'}
-        ]
+app.get('/todos', function (req, res) {
+    res.render('todos/index', {
+        todos: todos
     });
 })
 
-app.listen(port, function(){
-    console.log('server listening on port', port); 
+app.get('/todos/search', (req, res) => {
+    var q = req.query.q;
+    var matchedTodos = todos.filter((todo) => {
+        return todo.todo.toLocaleLowerCase().indexOf(q.toLocaleLowerCase()) !== -1;
+    })
+
+    res.render('todos/index', { 
+        todos: matchedTodos
+    })
+    console.log(req.query);
+})
+
+app.listen(port, function () {
+    console.log('server listening on port', port);
 })
 
