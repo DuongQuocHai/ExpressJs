1c5a5ab5ff4fc3d7eee9812060eaa8b067bbcdc6
= <=>
2020-08-02 16:57:51 +0700

    express lesson 2 - Template engines

--- WebServerNodeJS&&ExpressJS/index.js
+++ WebServerNodeJS&&ExpressJS/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const app = express();
+
+// const low = require('lowdb')
+// const FileSync = require('lowdb/adapters/FileSync')
+// const adapter = new FileSync('db.json')
+// const db = low(adapter)
+// db.defaults({ todos: []}).write()
+
 const port = 3000
-
-const todos = "<ul><li>Đi chợ</li><li>Nấu cơm</li><li>Rửa bát</li><li>Học code tại codersX</li></ul>";
-
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 
 app.get('/', function(req, res){
-    res.send("<h1>hello</h1><a href='/todos'>todos</>");
+    res.render("index", {
+        name: 'Hai'
+    });
 })
 
 app.get('/todos', function(req, res){
-    res.send(todos);
+    res.render('./todos/index',{
+        todos:[
+            {id:1, todo:'Đi chợ'},
+            {id:2, todo:'Nấu cơm'},
+            {id:3, todo:'Rửa bát'},
+            {id:4, todo:'Học tại CodersX'}
+        ]
+    });
 })
 
 app.listen(port, function(){
     console.log('server listening on port', port); 
 })
+
